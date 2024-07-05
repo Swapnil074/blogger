ae8cea53ffc3eb12f9af19575711942d1af889e7
Swapnil074 <mailswapnil0074@gmail.com>
2024-07-05 22:12:57 +0530

    signin route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -30,8 +30,25 @@ try{
   return c.status(403)
 }
 })
-app.post('/api/v1/user/signin', (c) => {
-  return c.text('signin route')
+app.post('/api/v1/user/signin', async (c) => {
+
+  const prisma=new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const body=await c.req.json()
+  const user=await prisma.user.findUnique({
+    where:{
+      email:body.email
+    }
+  })
+
+  if(!user){
+     c.status(403)
+    return c.json({message:'user not found'})
+    }
+  const jwt=await sign({id:user.id}, c.env.JWT_SECRET)
+  return c.json({jwt})
 })
 app.post('/api/v1/blog', (c) => {
   return c.text('blog route')
